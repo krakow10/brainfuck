3137829084bbab0516cd6a2f2f6d8f63968a5dd8
Quaternions <krakow20@gmail.com>
2024-03-14 15:01:31 -0700

    rework error handling

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ enum Instruction{
 	CloseLoop(usize),
 }
 #[derive(Debug)]
-enum Error{
+pub enum LexError{
 	InvalidInstruction(u8),
-	UnmatchedOpenLoop(usize),
-	UnmatchedCloseLoop(usize),
+	UnmatchedOpenLoop{position:usize},
+	UnmatchedCloseLoop{position:usize},
 }
 
 #[derive(Debug)]
@@ -26,7 +26,7 @@ struct Brainfuck{
 	data_head:usize,
 }
 impl TryFrom<&str> for Brainfuck{
-	type Error=Error;
+	type Error=LexError;
 	fn try_from(value:&str)->Result<Self,Self::Error>{
 		let mut stack=Vec::new();
 		let mut first_pass=Vec::with_capacity(value.len());
@@ -40,21 +40,27 @@ impl TryFrom<&str> for Brainfuck{
 				b','=>Some(Instruction::Read),
 				b'['=>{stack.push(i);None},
 				b']'=>{
-					let open_loop_index=stack.pop().ok_or(Error::UnmatchedCloseLoop(i))?;
+					let open_loop_index=stack.pop().ok_or(LexError::UnmatchedCloseLoop{position:i})?;
 					*first_pass.get_mut(open_loop_index).unwrap()=Some(Instruction::OpenLoop(i));
 					Some(Instruction::CloseLoop(open_loop_index))
 				},
-				other=>return Err(Error::InvalidInstruction(other)),
+				other=>return Err(LexError::InvalidInstruction(other)),
 			};
 			first_pass.push(ins);
 		}
 		Ok(Brainfuck{
-			instructions:first_pass.into_iter().collect::<Option<Vec<_>>>().ok_or_else(||Error::UnmatchedOpenLoop(stack.pop().unwrap()))?,
+			instructions:first_pass.into_iter().collect::<Option<Vec<_>>>().ok_or_else(||LexError::UnmatchedOpenLoop{position:stack[0]})?,
 			instruction_head:0,
 			data:Vec::new(),
 			data_head:0,
 		})
 	}
+}
+#[derive(Debug)]
+pub enum RunError{
+	Io(std::io::Error),
+	PointerOverflow{position:usize},
+	PointerUnderflow{position:usize},
 }
 impl Brainfuck{
 	fn get_or_reserve(&mut self)->&mut u8{
@@ -63,11 +69,18 @@ impl Brainfuck{
 		}
 		self.data.get_mut(self.data_head).unwrap()
 	}
-	fn step(&mut self)->bool{
+	/// Returns Result<should_continue>
+	fn step(&mut self)->Result<bool,RunError>{
 		let ins=self.instructions.get(self.instruction_head);
 		match ins{
-			Some(Instruction::MoveRight)=>self.data_head+=1,
-			Some(Instruction::MoveLeft)=>self.data_head-=1,
+			Some(Instruction::MoveRight)=>match self.data_head.checked_add(1){
+				Some(value)=>self.data_head=value,
+				None=>return Err(RunError::PointerOverflow{position:self.instruction_head}),
+			},
+			Some(Instruction::MoveLeft)=>match self.data_head.checked_sub(1){
+				Some(value)=>self.data_head=value,
+				None=>return Err(RunError::PointerUnderflow{position:self.instruction_head}),
+			},
 			Some(Instruction::Increment)=>{
 				let c=self.get_or_reserve();
 				*c=c.wrapping_add(1);
@@ -77,10 +90,10 @@ impl Brainfuck{
 				*c=c.wrapping_sub(1);
 			},
 			Some(Instruction::Write)=>{
-				std::io::stdout().write(std::slice::from_ref(self.get_or_reserve())).unwrap();
+				std::io::stdout().write(std::slice::from_ref(self.get_or_reserve())).map_err(RunError::Io)?;
 			},
 			Some(Instruction::Read)=>{
-				std::io::stdin().read_exact(std::slice::from_mut(self.get_or_reserve())).unwrap();
+				std::io::stdin().read_exact(std::slice::from_mut(self.get_or_reserve())).map_err(RunError::Io)?;
 			},
 			Some(&Instruction::OpenLoop(index))=>if *self.get_or_reserve()==0{
 				self.instruction_head=index;
@@ -88,19 +101,21 @@ impl Brainfuck{
 			Some(&Instruction::CloseLoop(index))=>if *self.get_or_reserve()!=0{
 				self.instruction_head=index;
 			},
-			None=>return false,
+			None=>return Ok(false),
 		}
 		self.instruction_head+=1;
-		true
+		Ok(true)
 	}
-	fn run(&mut self){
-		while self.step(){}
-		std::io::stdout().flush().unwrap();
+	fn run(&mut self)->Result<(),RunError>{
+		while self.step()?{}
+		std::io::stdout().flush().map_err(RunError::Io)?;
+		Ok(())
 	}
 }
 
 
 fn main(){
-	let mut bf=Brainfuck::try_from("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.").unwrap();
-	bf.run();
+	Brainfuck::try_from(
+		"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+	).unwrap().run().unwrap();
 }
